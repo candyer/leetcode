bca358341386bbcb35b56084e2937e7b22ef4c0b
J C <candyer@users.noreply.github.com>
2018-01-15 00:02:31 +0100

    747. Largest Number At Least Twice of Others

---
+++ dominantIndex.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/largest-number-at-least-twice-of-others/description/
+# 747. Largest Number At Least Twice of Others
+
+# In a given integer array nums, there is always exactly one largest element.
+# Find whether the largest element in the array is at least twice as much as every other number in the array.
+# If it is, return the index of the largest element, otherwise return -1.
+
+# Example 1:  Input: nums = [3, 6, 1, 0]  Output: 1
+# Explanation: 6 is the largest integer, and for every other number in the array x,
+# 6 is more than twice as big as x.  The index of value 6 is 1, so we return 1.
+
+# Example 2:  Input: nums = [1, 2, 3, 4]  Output: -1
+# Explanation: 4 isn't at least as big as twice the value of 3, so we return -1.
+
+# Note:
+# nums will have a length in the range [1, 50].
+# Every nums[i] will be an integer in the range [0, 99].
+
+
+def dominantIndex(nums):
+	"""
+	:type nums: List[int]
+	:rtype: int
+	"""
+	if len(nums) == 1: return 0
+	array = sorted(nums)
+	if array[-1] / 2 >= array[-2]:
+		return nums.index(array[-1])
+	return -1
+
+assert dominantIndex([3, 6, 1, 0]) == 1
+assert dominantIndex([1, 2, 3, 4]) == -1
+assert dominantIndex([5, 10]) == 1
+assert dominantIndex([5, 0]) == 0
+assert dominantIndex([5]) == 0
+
+
