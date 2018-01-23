9801b9e991c1cf4ee9ea7f03e0b0034eaf6b0715
cc <candyer@users.noreply.github.com>
2018-01-23 15:27:32 +0100

    657. Judge Route Circle

---
+++ judgeCircle.py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/judge-route-circle/description/
+
+
+# 657. Judge Route Circle
+
+# Initially, there is a Robot at position (0, 0). Given a sequence of its moves, 
+# judge if this robot makes a circle, which means it moves back to the original place.
+
+# The move sequence is represented by a string. And each move is represent by a 
+# character. The valid robot moves are R (Right), L (Left), U (Up) and D (down). 
+# The output should be true or false representing whether the robot makes a circle.
+
+# Example 1:  Input: "UD"  Output: true
+# Example 2:  Input: "LL"  Output: false
+
+
+def judgeCircle(moves):
+	"""
+	:type moves: str
+	:rtype: bool
+	"""
+	direction = {
+		'U': [0, 1], 
+		'R': [1, 0], 
+		'D': [0, -1],
+		'L': [-1, 0]
+	}
+
+	x, y = 0, 0
+	for move in moves:
+		x1, y1 = direction[move]
+		x += x1
+		y += y1
+	return x == y == 0
+
+
+
+assert judgeCircle('UD') == True
+assert judgeCircle('LL') == False
