fbfa4041287344ed041a2bfbdd235332706a99c7
cc <candyer@users.noreply.github.com>
2018-03-29 16:57:09 +0200

    605. Can Place Flowers

--- canPlaceFlowers.py
+++ canPlaceFlowers.py
@@ -19,57 +19,81 @@
 # The input array size is in the range of [1, 20000].
 # n is a non-negative integer which won't exceed the input array size.
 
+#####################
+### solution 1 ######
+#####################
+# def canPlaceFlowers(flowerbed, n):
+# 	"""
+# 	:type flowerbed: List[int]
+# 	:type n: int
+# 	:rtype: bool
+# 	"""
+# 	m = len(flowerbed)
+# 	start = end = False
+# 	begin = 0
+# 	if flowerbed[0] == 0:
+# 		start = True
+# 	if flowerbed[-1] == 0:
+# 		end = True
+
+# 	if 1 in flowerbed:
+# 		begin = flowerbed.index(1)
+# 		if begin != 0:
+# 			start = True
+# 	else:
+# 		if n <= (m + 1) / 2:
+# 			return True
+# 		return False
+
+# 	n -= begin / 2
+# 	if n <= 0:
+# 		return True
+	
+# 	i = begin
+# 	while i < m:
+# 		j = i
+# 		count = 1
+# 		while flowerbed[j] == 0 and j < m - 1:			
+# 			j += 1
+# 			if j == m - 1 and flowerbed[j] == 0 and end:
+# 				n -= (count + 1) / 2
+# 			elif flowerbed[j] == 1:
+# 				n -= (count - 1) / 2
+# 			if n <= 0:
+# 				return True
+# 				break
+# 			count += 1
+# 		i += count 
+# 	return False
+
+
+#####################
+### solution 2 ######
+#####################
 
 def canPlaceFlowers(flowerbed, n):
-	"""
-	:type flowerbed: List[int]
-	:type n: int
-	:rtype: bool
-	"""
-	m = len(flowerbed)
-	start = end = False
-	begin = 0
-	if flowerbed[0] == 0:
-		start = True
-	if flowerbed[-1] == 0:
-		end = True
-
-	if 1 in flowerbed:
-		begin = flowerbed.index(1)
-		if begin != 0:
-			start = True
-	else:
-		if n <= (m + 1) / 2:
-			return True
-		return False
-
-	n -= begin / 2
-	if n <= 0:
-		return True
-	
-	i = begin
-	while i < m:
-		j = i
-		count = 1
-		while flowerbed[j] == 0 and j < m - 1:			
-			j += 1
-			if j == m - 1 and flowerbed[j] == 0 and end:
-				n -= (count + 1) / 2
-			elif flowerbed[j] == 1:
+	count = 1
+	for bed in flowerbed:
+		if bed == 1:
+			if count:
 				n -= (count - 1) / 2
-			if n <= 0:
-				return True
-				break
+			count = 0
+		else:
 			count += 1
-		i += count 
-	return False
+	n -= count / 2
+	return n <= 0
 
 
-print canPlaceFlowers([1,0,0,0,1,0,0], 2) == True
-print canPlaceFlowers([1,0,0,0,0,1], 2) == False
-print canPlaceFlowers([1,0,0,0,1], 1) == True
-print canPlaceFlowers([0,0,1,0,0,0,1,0,0], 4) == False
-print canPlaceFlowers([0,0,1,0,0,0,1,0,0], 3) == True
-print canPlaceFlowers([0], 1) == True
-print canPlaceFlowers([0,0,0,0,0], 3) == True
-print canPlaceFlowers([1,0,0,0,1], 2) == False
+
+
+
+
+
+assert canPlaceFlowers([1,0,0,0,1,0,0], 2) == True
+assert canPlaceFlowers([1,0,0,0,0,1], 2) == False
+assert canPlaceFlowers([1,0,0,0,1], 1) == True
+assert canPlaceFlowers([0,0,1,0,0,0,1,0,0], 4) == False
+assert canPlaceFlowers([0,0,1,0,0,0,1,0,0], 3) == True
+assert canPlaceFlowers([0], 1) == True
+assert canPlaceFlowers([0,0,0,0,0], 3) == True
+assert canPlaceFlowers([1,0,0,0,1], 2) == False
