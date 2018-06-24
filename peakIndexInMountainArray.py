14b050a23b412e8d84aaf0605a639f068f7003f2
c <candyer@users.noreply.github.com>
2018-06-24 20:17:56 +0200

    852. Peak Index in a Mountain Array

---
+++ peakIndexInMountainArray.py
@@ -0,0 +1,48 @@
+# https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+
+# 852. Peak Index in a Mountain Array
+
+# Let's call an array A a mountain if the following properties hold:
+
+# A.length >= 3
+# There exists some 0 < i < A.length - 1 
+# such that A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1]
+# Given an array that is definitely a mountain, return any i 
+# such that A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1].
+
+# Example 1:
+# Input: [0,1,0]
+# Output: 1
+
+# Example 2:
+# Input: [0,2,1,0]
+# Output: 1
+
+# Note:
+# 3 <= A.length <= 10000
+# 0 <= A[i] <= 10^6
+# A is a mountain, as defined above.
+
+
+
+def peakIndexInMountainArray(A):
+	"""
+	:type A: List[int]
+	:rtype: int
+	"""
+	maxi = max(A)
+	return A.index(maxi)
+
+
+assert peakIndexInMountainArray([0,1,0]) == 1
+assert peakIndexInMountainArray([0,2,1,0]) == 1
+assert peakIndexInMountainArray([100,2,10,1,10,0,110]) == 6
+assert peakIndexInMountainArray([110,2,10,1,10,0,100]) == 0
+
+
+
+
+
+
+
+
