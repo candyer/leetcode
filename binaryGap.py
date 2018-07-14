2dc4e22a9561564d65080663d3744108ba10f5ed
c <candyer@users.noreply.github.com>
2018-07-15 00:33:23 +0200

    868. Binary Gap

---
+++ binaryGap.py
@@ -0,0 +1,63 @@
+# https://leetcode.com/problems/binary-gap/description/
+
+# 868. Binary Gap
+
+# Given a positive integer N, find and return the longest distance between two consecutive 1's in the binary representation of N.
+# If there aren't two consecutive 1's, return 0.
+
+# Example 1:
+# Input: 22
+# Output: 2
+# Explanation: 
+# 22 in binary is 0b10110.
+# In the binary representation of 22, there are three ones, and two consecutive pairs of 1's.
+# The first consecutive pair of 1's have distance 2.
+# The second consecutive pair of 1's have distance 1.
+# The answer is the largest of these two distances, which is 2.
+
+# Example 2:
+# Input: 5
+# Output: 2
+# Explanation: 
+# 5 in binary is 0b101.
+
+# Example 3:
+# Input: 6
+# Output: 1
+# Explanation: 
+# 6 in binary is 0b110.
+
+# Example 4:
+# Input: 8
+# Output: 0
+# Explanation: 
+# 8 in binary is 0b1000.
+# There aren't any consecutive pairs of 1's in the binary representation of 8, so we return 0.
+ 
+# Note:
+# 1 <= N <= 10^9
+
+
+def binaryGap(N):
+	"""
+	:type N: int
+	:rtype: int
+	"""
+	binary = bin(N)[2:]
+	res = pre = curr = 0
+	for i in range(len(binary)):
+		if binary[i] == '1':
+			curr = i
+			res = max(res, curr - pre)
+			pre = curr
+	return  res
+
+
+assert binaryGap(22) == 2
+assert binaryGap(5) == 2
+assert binaryGap(6) == 1
+assert binaryGap(8) == 0
+assert binaryGap(1234567890) == 8
+
+
+
