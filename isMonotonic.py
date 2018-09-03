0c9f73f94ea1a04475e6c2b6c60447b3b43e380c
c <candyer@users.noreply.github.com>
2018-09-03 23:48:19 +0200

    896. Monotonic Array

---
+++ isMonotonic.py
@@ -0,0 +1,60 @@
+# https://leetcode.com/problems/monotonic-array/description/
+
+# 896. Monotonic Array
+
+# An array is monotonic if it is either monotone increasing or monotone decreasing.
+# An array A is monotone increasing if for all i <= j, A[i] <= A[j].  An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
+# Return true if and only if the given array A is monotonic.
+ 
+# Example 1:
+# Input: [1,2,2,3]
+# Output: true
+
+# Example 2:
+# Input: [6,5,4,4]
+# Output: true
+
+# Example 3:
+# Input: [1,3,2]
+# Output: false
+
+# Example 4:
+# Input: [1,2,4,5]
+# Output: true
+
+# Example 5:
+# Input: [1,1,1]
+# Output: true
+ 
+# Note:
+# 1 <= A.length <= 50000
+# -100000 <= A[i] <= 100000
+
+
+def isMonotonic(A):
+	"""
+	:type A: List[int]
+	:rtype: bool
+	"""
+	asc = sorted(A)
+	desc = asc[::-1]
+	if A == asc or A == desc:
+		return True
+	return False
+
+assert isMonotonic([1,2,2,3]) == True
+assert isMonotonic([6,5,4,4]) == True
+assert isMonotonic([1,3,2]) == False
+assert isMonotonic([1,2,4,5]) == True
+assert isMonotonic([1,1,1]) == True
+
+
+
+
+
+
+
+
+
+
+
