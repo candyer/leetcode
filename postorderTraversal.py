c7a1125850dd9fa7453bea493db3d9b5b5564696
c <candyer@users.noreply.github.com>
2019-04-23 23:46:23 +0200

    145. Binary Tree Postorder Traversal

---
+++ postorderTraversal.py
@@ -0,0 +1,67 @@
+# https://leetcode.com/problems/binary-tree-postorder-traversal/
+
+
+# 145. Binary Tree Postorder Traversal
+
+# Given a binary tree, return the postorder traversal of its nodes' values.
+
+# Example:
+
+# Input: [1,null,2,3]
+#    1
+#     \
+#      2
+#     /
+#    3
+
+# Output: [3,2,1]
+# Follow up: Recursive solution is trivial, could you do it iteratively?
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def dfs(self, root, res):
+        if root:
+            self.dfs(root.left, res)
+            self.dfs(root.right, res)
+            res.append(root.val)
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        self.dfs(root, res)
+        return res 
+
+
+
+###################################################
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        stack, res = [root], []
+        while stack:
+            node = stack.pop()
+            if node:
+                res.append(node.val)
+                stack.append(node.left)
+                stack.append(node.right)
+        return res[::-1]              
