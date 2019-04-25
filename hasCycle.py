de10b1e22522d6ed5e0c8050728c3fb98f01c344
c <candyer@users.noreply.github.com>
2019-04-26 00:15:32 +0200

    141. Linked List Cycle

--- hasCycle.py
+++ hasCycle.py
@@ -34,6 +34,8 @@
 #         self.val = x
 #         self.next = None
 
+
+################### hash table ####################
 class Solution(object):
 	def hasCycle(self, head):
 		"""
@@ -48,4 +50,60 @@ class Solution(object):
 				visited.add(head)
 			head = head.next
 		return False
+
+
+################### two pointer ####################
+class Solution(object):
+	def hasCycle(self, head):
+		"""
+		:type head: ListNode
+		:rtype: bool
+		"""
+		if head == None or head.next == None:
+			return False
+		slow, fast = head, head.next
+
+		while slow != fast:
+			if fast == None or fast.next == None:
+				return False
+			slow = slow.next
+			fast = fast.next.next
+		return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
