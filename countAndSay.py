72dfcbb229bb576bec81911fc6d5ba6bba3774d2
c <candyer@users.noreply.github.com>
2019-05-16 00:09:53 +0200

    38. Count and Say

--- countAndSay.py
+++ countAndSay.py
@@ -26,35 +26,59 @@
 # Input: 4
 # Output: "1211"
 
-def f(num):
-	array = list(num) + ['*']
-	n = len(array)
-	res = []
-	count = 1
-	for i in range(n - 1):
-		if array[i] == array[i + 1] :
-			count += 1
-		else:
-			res.append(str(count))
-			res.append(array[i])
-			count = 1
-	return ''.join(res)
+# def f(num):
+# 	array = list(num) + ['*']
+# 	n = len(array)
+# 	res = []
+# 	count = 1
+# 	for i in range(n - 1):
+# 		if array[i] == array[i + 1] :
+# 			count += 1
+# 		else:
+# 			res.append(str(count))
+# 			res.append(array[i])
+# 			count = 1
+# 	return ''.join(res)
 
 
-def solve():
-	res = ['1', '11', '21', '1211', '111221']
-	for i in range(6, 31):
-		res.append(f(res[i - 2]))
-	return res
+# def solve():
+# 	res = ['1', '11', '21', '1211', '111221']
+# 	for i in range(6, 31):
+# 		res.append(f(res[i - 2]))
+# 	return res
+
+# def countAndSay(n):
+# 	"""
+# 	:type n: int
+# 	:rtype: str
+# 	"""
+# 	return solve()[n - 1]
+
 
 def countAndSay(n):
 	"""
 	:type n: int
 	:rtype: str
 	"""
-	return solve()[n - 1]
+	res = '1'
+	for i in range(n - 1):
+		cur = res[0]
+		count = 1
+		tmp = []
+		for i in range(1, len(res)):
+			if res[i] == cur:
+				count += 1
+				continue
+			tmp.append(str(count))
+			tmp.append(cur)
+			cur = res[i]
+			count = 1
+		tmp.append(str(count))
+		tmp.append(cur)
+		res = ''.join(tmp)
+	return res
 
-
+assert countAndSay(5) == '111221'
 assert countAndSay(7) == '13112221'
 assert countAndSay(10) == '13211311123113112211'
 
