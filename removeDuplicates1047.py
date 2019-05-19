e2ece36a8e1039584edc36b8f5047c1fe74f09bb
c <candyer@users.noreply.github.com>
2019-05-19 18:44:20 +0200

    1047. Remove All Adjacent Duplicates In String

---
+++ removeDuplicates1047.py
@@ -0,0 +1,50 @@
+# https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/description/
+
+
+# 1047. Remove All Adjacent Duplicates In String
+
+# Given a string S of lowercase letters, a duplicate removal consists of choosing two adjacent and equal letters, and removing them.
+
+# We repeatedly make duplicate removals on S until we no longer can.
+
+# Return the final string after all such duplicate removals have been made.  It is guaranteed the answer is unique.
+
+ 
+
+# Example 1:
+
+# Input: "abbaca"
+# Output: "ca"
+# Explanation: 
+# For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  
+# The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+ 
+
+# Note:
+
+# 1 <= S.length <= 20000
+# S consists only of English lowercase letters.
+
+
+def removeDuplicates(S):
+	"""
+	:type S: str
+	:rtype: str
+	"""
+	stack = [S[0]]
+	n, i = len(S), 1
+	while i < n:
+		while stack and i < n and S[i] == stack[-1]:
+			stack.pop()
+			i += 1
+		if i < n:
+			stack.append(S[i])
+		i += 1
+	return ''.join(stack)
+
+
+
+assert removeDuplicates('abbaca') == 'ca'
+assert removeDuplicates('abbbcddcaax') == 'abx'
+assert removeDuplicates('abbbcddcaa') == 'ab'
+assert removeDuplicates('abbbbaa') == 'a'
