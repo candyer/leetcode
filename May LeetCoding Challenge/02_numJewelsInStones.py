922e268d8e9571d041e3333d41ab1505bbb69788
c <candyer@users.noreply.github.com>
2020-05-06 12:56:40 +0200

    Jewels and Stones

---
+++ May LeetCoding Challenge/02_numJewelsInStones.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3317/
+
+# You're given strings J representing the types of stones that are jewels, 
+# and S representing the stones you have.  Each character in S is a type of stone you have.  
+# You want to know how many of the stones you have are also jewels.
+
+# The letters in J are guaranteed distinct, and all characters in J and S are letters. 
+# Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+
+# Example 1:
+# Input: J = "aA", S = "aAAbbbb"
+# Output: 3
+
+# Example 2:
+# Input: J = "z", S = "ZZ"
+# Output: 0
+
+# Note:
+# S and J will consist of letters and have length at most 50.
+# The characters in J are distinct.
+
+from collections import Counter
+def numJewelsInStones(J: str, S: str) -> int:
+    d = Counter(S)
+    res = 0
+    for k, v in d.items():
+        if k in J:
+            res += v
+    return res
+
+assert(numJewelsInStones("aA", "aAAbbbb") == 3)
+assert(numJewelsInStones("z", "ZZ") == 0)
+
