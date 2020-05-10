195b2594aaf848b28a81d000350a07d629be6c47
c <candyer@users.noreply.github.com>
2020-05-10 13:57:27 +0200

    Find the Town Judge

---
+++ May LeetCoding Challenge/10_findJudge.py
@@ -0,0 +1,76 @@
+# https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3325/
+
+# In a town, there are N people labelled from 1 to N.  There is a rumor
+# that one of these people is secretly the town judge.
+
+# If the town judge exists, then:
+
+# The town judge trusts nobody.
+# Everybody (except for the town judge) trusts the town judge.
+# There is exactly one person that satisfies properties 1 and 2.
+# You are given trust, an array of pairs trust[i] = [a, b] 
+# representing that the person labelled a trusts the person labelled b.
+
+# If the town judge exists and can be identified, 
+# return the label of the town judge.  Otherwise, return -1.
+
+ 
+# Example 1:
+# Input: N = 2, trust = [[1,2]]
+# Output: 2
+
+# Example 2:
+# Input: N = 3, trust = [[1,3],[2,3]]
+# Output: 3
+
+# Example 3:
+# Input: N = 3, trust = [[1,3],[2,3],[3,1]]
+# Output: -1
+
+# Example 4:
+# Input: N = 3, trust = [[1,2],[2,3]]
+# Output: -1
+
+# Example 5:
+# Input: N = 4, trust = [[1,3],[1,4],[2,3],[2,4],[4,3]]
+# Output: 3
+ 
+
+# Note:
+# 1 <= N <= 1000
+# trust.length <= 10000
+# trust[i] are all different
+# trust[i][0] != trust[i][1]
+# 1 <= trust[i][0], trust[i][1] <= N
+
+
+from collections import Counter
+from typing import List
+def findJudge(N: int, trust: List[List[int]]) -> int:
+	if N == 1 and trust == []:
+		return 1
+	d1 = Counter()
+	d2 = Counter()
+	for a, b in trust:
+		d1[b] += 1
+		d2[a] += 1
+
+	for k, v in d1.items():
+		if v == N - 1 and d2[k] == 0:
+			return k
+	return -1
+
+
+assert(findJudge(1, []) == 1)
+assert(findJudge(2, [[1,2]]) == 2)
+assert(findJudge(3, [[1,3],[2,3]]) == 3)
+assert(findJudge(3, [[1,3],[2,3],[3,1]]) == -1)
+assert(findJudge(3, [[1,2],[2,3]]) == -1)
+assert(findJudge(4, [[1,3],[1,4],[2,3],[2,4],[4,3]]) == 3)
+
+
+
+
+
+
+
