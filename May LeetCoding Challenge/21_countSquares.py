7869417caf97adf29a852775c01c1a299bc92bd6
c <candyer@users.noreply.github.com>
2020-05-22 00:58:25 +0200

    Count Square Submatrices with All Ones

---
+++ May LeetCoding Challenge/21_countSquares.py
@@ -0,0 +1,62 @@
+# https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3336/
+
+# Count Square Submatrices with All Ones
+
+# Given a m * n matrix of ones and zeros, return how many square submatrices have all ones.
+
+
+# Example 1:
+# Input: matrix =
+# [
+#   [0,1,1,1],
+#   [1,1,1,1],
+#   [0,1,1,1]
+# ]
+# Output: 15
+# Explanation: 
+# There are 10 squares of side 1.
+# There are 4 squares of side 2.
+# There is  1 square of side 3.
+# Total number of squares = 10 + 4 + 1 = 15.
+
+# Example 2:
+# Input: matrix = 
+# [
+#   [1,0,1],
+#   [1,1,0],
+#   [1,1,0]
+# ]
+# Output: 7
+# Explanation: 
+# There are 6 squares of side 1.  
+# There is 1 square of side 2. 
+# Total number of squares = 6 + 1 = 7.
+ 
+
+# Constraints:
+# 1 <= arr.length <= 300
+# 1 <= arr[0].length <= 300
+# 0 <= arr[i][j] <= 1
+
+
+from typing import List
+def countSquares(matrix: List[List[int]]) -> int:
+	for r in range(1, len(matrix)):
+		for c in range(1, len(matrix[0])):
+			if matrix[r][c] == 1:
+				matrix[r][c] = min(matrix[r - 1][c - 1], matrix[r - 1][c], matrix[r][c - 1]) + 1
+	return sum(map(sum, matrix))
+
+
+assert(countSquares([
+	[0,1,1,1],
+	[1,1,1,1],
+	[0,1,1,1]]) == 15)
+assert(countSquares([
+	[1,0,1],
+	[1,1,0],
+	[1,1,0]]) == 7)
+
+
+
+
