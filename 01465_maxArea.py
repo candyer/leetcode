09b0d01cdf2fe9fe746165736c84ecb71f66d1ca
c <candyer@users.noreply.github.com>
2020-06-04 23:03:47 +0200

    1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts

---
+++ 01465_maxArea.py
@@ -0,0 +1,61 @@
+# https://leetcode.com/problems/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/description/
+
+# 1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts
+# Given a rectangulat h and width w, and two arrays of integers horizontalCuts and 
+# verticalCuts where horizontalCuts[i] is the distance from the top of the rectangular cake to the 
+# ith horizontal cut and similarly, verticalCuts[j] is the distance from the left of the rectangular 
+# cake to the jth vertical cut.
+
+# Return the maximum area of a piece of cake after you cut at each horizontal and vertical position 
+# provided in the arrays horizontalCuts and verticalCuts. Since the answer can be a huge number, 
+# return this modulo 10^9 + 7.
+
+
+# Example 1:
+# Input: h = 5, w = 4, horizontalCuts = [1,2,4], verticalCuts = [1,3]
+# Output: 4 
+# Explanation: The figure above represents the given rectangular cake. Red lines are the horizontal 
+# and vertical cuts. After you cut the cake, the green piece of cake has the maximum area.
+
+# Example 2:
+# Input: h = 5, w = 4, horizontalCuts = [3,1], verticalCuts = [1]
+# Output: 6
+# Explanation: The figure above represents the given rectangular cake. Red lines are the horizontal 
+# and vertical cuts. After you cut the cake, the green and yellow pieces of cake have the maximum area.
+
+# Example 3:
+# Input: h = 5, w = 4, horizontalCuts = [3], verticalCuts = [3]
+# Output: 9
+ 
+
+# Constraints:
+# 2 <= h, w <= 10^9
+# 1 <= horizontalCuts.length < min(h, 10^5)
+# 1 <= verticalCuts.length < min(w, 10^5)
+# 1 <= horizontalCuts[i] < h
+# 1 <= verticalCuts[i] < w
+# It is guaranteed that all elements in horizontalCuts are distinct.
+# It is guaranteed that all elements in verticalCuts are distinct.
+
+
+from typing import List
+def get_max_block(n: int, array: List[int]) -> int:
+	array.extend([0, n])
+	array.sort()
+	res = 0
+	for i in range(len(array) - 1):
+		res = max(res, array[i + 1] - array[i])
+	return res
+
+def maxArea(h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
+	mod = 10**9 + 7
+	row_max_block = get_max_block(h, horizontalCuts)
+	col_max_block = get_max_block(w, verticalCuts)
+	return row_max_block * col_max_block % mod
+
+
+assert(maxArea(5, 4, [1,2,4], [1,3]) == 4)
+assert(maxArea(5, 4, [3,1], [1]) == 6)
+assert(maxArea(5, 4, [3], [3]) == 9)
+
+
