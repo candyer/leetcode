b6da238cff49a72b3676d961f5a955710d6c1ce3
c <candyer@users.noreply.github.com>
2020-06-07 21:18:47 +0200

    Coin Change 2

---
+++ 2020 June LeetCoding Challenge/07_change.py
@@ -0,0 +1,52 @@
+# https://leetcode.com/explore/featured/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3353/
+
+# Coin Change 2
+
+# You are given coins of different denominations and a total amount of money. 
+# Write a function to compute the number of combinations that make up that amount. 
+# You may assume that you have infinite number of each kind of coin.
+
+
+# Example 1:
+# Input: amount = 5, coins = [1, 2, 5]
+# Output: 4
+# Explanation: there are four ways to make up the amount:
+# 5=5
+# 5=2+2+1
+# 5=2+1+1+1
+# 5=1+1+1+1+1
+
+# Example 2:
+# Input: amount = 3, coins = [2]
+# Output: 0
+# Explanation: the amount of 3 cannot be made up just with coins of 2.
+
+# Example 3:
+# Input: amount = 10, coins = [10] 
+# Output: 1
+ 
+
+# Note:
+# You can assume that
+# 0 <= amount <= 5000
+# 1 <= coin <= 5000
+# the number of coins is less than 500
+# the answer is guaranteed to fit into signed 32-bit integer
+
+from typing import List
+def change(amount: int, coins: List[int]) -> int:
+	dp = [0] * (amount + 1)
+	dp[0] = 1
+	for coin in coins:
+		for i in range(1, amount + 1):
+			if coin <= i:
+				dp[i] += dp[i - coin]
+	return dp[-1]
+
+assert(change(5, [1, 2, 5]) == 4)
+assert(change(3, [2]) == 0)
+assert(change(10, [10]) == 1)
+assert(change(10, [1, 2, 5, 7]) == 12)
+
+
+
