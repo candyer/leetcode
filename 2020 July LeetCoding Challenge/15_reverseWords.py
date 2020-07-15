15d8a279d6886d1a5a01bff2e5aacb3ce375fb0d
c <candyer@users.noreply.github.com>
2020-07-15 10:19:52 +0200

    Reverse Words in a String

---
+++ 2020 July LeetCoding Challenge/15_reverseWords.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/546/week-3-july-15th-july-21st/3391/
+
+# Reverse Words in a String
+
+# Given an input string, reverse the string word by word.
+
+# Example 1:
+# Input: "the sky is blue"
+# Output: "blue is sky the"
+
+# Example 2:
+# Input: "  hello world!  "
+# Output: "world! hello"
+# Explanation: Your reversed string should not contain leading or trailing spaces.
+
+# Example 3:
+# Input: "a good   example"
+# Output: "example good a"
+# Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ 
+
+# Note:
+# A word is defined as a sequence of non-space characters.
+# Input string may contain leading or trailing spaces. However, 
+# your reversed string should not contain leading or trailing spaces.
+# You need to reduce multiple spaces between two words to a single space in the reversed string.
+ 
+
+# Follow up:
+# For C programmers, try to solve it in-place in O(1) extra space.
+
+
+def reverseWords(s: str) -> str:
+	return ' '.join(s.split()[::-1])
+
+assert(reverseWords("the sky is blue") == 'blue is sky the')
+assert(reverseWords("  hello world!  ") == 'world! hello')
+assert(reverseWords("a good   example") == 'example good a')
