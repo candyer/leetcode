c2a6c2e15f8feb10efb965435bc488972a2eed25
c <candyer@users.noreply.github.com>
2020-10-06 18:31:59 +0200

    Insert into a Binary Search Tree

---
+++ 2020 October LeetCoding Challenge/06_insertIntoBST.py
@@ -0,0 +1,86 @@
+# https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3485/
+
+# Insert into a Binary Search Tree
+
+# You are given the root node of a binary search tree (BST) and a value to insert into the tree. 
+# Return the root node of the BST after the insertion. It is guaranteed that the new value does 
+# not exist in the original BST.
+
+# Notice that there may exist multiple valid ways for the insertion, as long as the tree remains 
+# a BST after insertion. You can return any of them.
+
+
+# Example 1:
+# Input: root = [4,2,7,1,3], val = 5
+# Output: [4,2,7,1,3,5]
+# Explanation: Another accepted tree is:
+
+
+# Example 2:
+# Input: root = [40,20,60,10,30,50,70], val = 25
+# Output: [40,20,60,10,30,50,70,null,null,25]
+
+# Example 3:
+# Input: root = [4,2,7,1,3,null,null,null,null,null,null], val = 5
+# Output: [4,2,7,1,3,5]
+ 
+
+# Constraints:
+# The number of nodes in the tree will be in the range [0, 104].
+# -108 <= Node.val <= 108
+# All the values Node.val are unique.
+# -108 <= val <= 108
+# It's guaranteed that val does not exist in the original BST.
+
+
+# Definition for a binary tree node.
+class TreeNode:
+	def __init__(self, val=0, left=None, right=None):
+		self.val = val
+		self.left = left
+		self.right = right
+
+###################################################################
+class Solution:
+	def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+		if not root:
+			return TreeNode(val)
+		curr_node = root
+		while True:
+			if val < curr_node.val:
+				if not curr_node.left:
+					curr_node.left = TreeNode(val)
+					break
+				else:
+					curr_node = curr_node.left
+			else:
+				if not curr_node.right:
+					curr_node.right = TreeNode(val)
+					break
+				else:
+					curr_node = curr_node.right    
+		return root
+
+
+
+
+###################################################################
+class Solution:
+	def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+		if not root:
+			return TreeNode(val)
+		if val < root.val:
+			root.left = self.insertIntoBST(root.left, val)
+		elif val > root.val:
+			root.right = self.insertIntoBST(root.right, val)
+		return root
+
+
+
+
+
+
+
+
+
+		
