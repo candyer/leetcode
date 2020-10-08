b56233a70f6582f9130235055f47236de25ea872
c <candyer@users.noreply.github.com>
2020-10-08 15:25:44 +0200

    Binary Search

---
+++ 2020 October LeetCoding Challenge/08_search.py
@@ -0,0 +1,50 @@
+# https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/560/week-2-october-8th-october-14th/3488/
+
+
+# Binary Search
+
+
+# Given a sorted (in ascending order) integer array nums of n elements and a target value, 
+# write a function to search target in nums. If target exists, then return its index, otherwise return -1.
+
+
+# Example 1:
+# Input: nums = [-1,0,3,5,9,12], target = 9
+# Output: 4
+# Explanation: 9 exists in nums and its index is 4
+
+# Example 2:
+# Input: nums = [-1,0,3,5,9,12], target = 2
+# Output: -1
+# Explanation: 2 does not exist in nums so return -1
+ 
+
+# Note:
+# You may assume that all elements in nums are unique.
+# n will be in the range [1, 10000].
+# The value of each element in nums will be in the range [-9999, 9999].
+
+
+from typing import List
+def search(nums: List[int], target: int) -> int:
+	left, right = 0, len(nums) - 1
+	while left <= right:
+		mid = (left + right) // 2
+		if target == nums[mid]:
+			return mid
+		elif target < nums[mid]: 
+			right = mid - 1
+		else:
+			left = mid + 1
+	return -1
+
+assert(search([-1,0,3,5,9,12], 9) == 4)
+assert(search([-1,0,3,5,9,12], 2) == -1)
+assert(search([10], 10) == 0)
+assert(search([10, 11], 11) == 1)
+
+
+
+
+
+
