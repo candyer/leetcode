1f112af7ad7d17fdb2fcc89a73a0e87926ffaa14
c <candyer@users.noreply.github.com>
2020-10-22 21:56:22 +0200

    Minimum Depth of Binary Tree

---
+++ 2020 October LeetCoding Challenge/22_minDepth.py
@@ -0,0 +1,77 @@
+# https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/562/week-4-october-22nd-october-28th/3504/
+
+# Minimum Depth of Binary Tree
+
+
+# Given a binary tree, find its minimum depth.
+# The minimum depth is the number of nodes along the shortest path 
+# from the root node down to the nearest leaf node.
+
+# Note: A leaf is a node with no children.
+
+
+# Example 1:
+# Input: root = [3,9,20,null,null,15,7]
+# Output: 2
+
+# Example 2:
+# Input: root = [2,null,3,null,4,null,5,null,6]
+# Output: 5
+ 
+
+# Constraints:
+# The number of nodes in the tree is in the range [0, 105].
+# -1000 <= Node.val <= 1000
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            return 1
+        if not root.left:
+            return self.minDepth(root.right) + 1
+        if not root.right:
+            return self.minDepth(root.left) + 1
+        return min(self.minDepth(root,left), self.minDepth(root.right)) + 1
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        stack = [root]
+        depth = 1
+        while stack:
+            tmp = []
+            for node in stack:
+                if not node.left and not node.right:
+                    return depth
+                if node.left:
+                    tmp.append(node.left)
+                if node.right:
+                    tmp.append(node.right)
+            stack = tmp
+            depth += 1
+        return depth
+
+
+
+
+
+
